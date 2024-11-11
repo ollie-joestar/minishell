f6f73024fecfac65716873aa8d84b852efa46ecd
HanzoK <hanju.soonsik.kim@gmail.com>
2024-11-11 16:04:55 +0100

    Refactoring and updating logic of quote functions and interpreting str

--- src/interpret.c
+++ src/interpret.c
@@ -1,10 +1,22 @@
 #include "minishell.h"
 
-void	cat_single_qoutes(char *str, char **start, char *end, size_t len)
+void	cat_quoted_string(t_data *data, char *str, char **start, char *end, char quote_type)
 {
 	*end = 0;
-	ft_strlcat(str, (*start) + 1, len + 1);
-	*end = '\'';
+	if (quote_type == '\'')
+		ft_strlcat(str, (*start) + 1, (end - *start));
+	else if (quote_type == '\"')
+	{
+		(*start)++;
+		while (*start < end)
+		{
+			if (possible_var(data, start[0][0], start[0][1]))
+				cat_env_var(data, str, start, end);
+			else
+				ft_strlcat(str, (*start)++, strlen(str) + 2);
+		}
+	}
+	*end = quote_type;
 	*start = end;
 }
 
@@ -29,7 +41,7 @@ void	cat_status(char *str, int status, size_t len)
 	ft_strlcat(str, s + (base - digits), len + 1);
 }
 
-void	cat_env_var(t_var *var, char *str, char **start, char *end)
+void	cat_env_var(t_data *data, char *str, char **start, char *end)
 {
 	size_t	i;
 	size_t	tkn_i;
@@ -39,9 +51,9 @@ void	cat_env_var(t_var *var, char *str, char **start, char *end)
 	if (start[0][1] == '?')
 	{
 		(*start) += 1;
-		cat_status(str, var->status, var->len);
+		cat_status(str, data->status, data->len);
 	}
-	else if (env_var_len(var, *start, end, &i))
+	else if (env_var_len(data, *start, end, &i))
 	{
 		(*start)++;
 		tmp = start[0][i];
@@ -55,37 +67,22 @@ void	cat_env_var(t_var *var, char *str, char **start, char *end)
 		*start += i;
 }
 
-void	cat_double_qoutes(t_var *var, char *str, char **start, char *end)
-{
-	*end = 0;
-	(*start)++;
-	while (*start < end)
-	{
-		if (possible_var(var, start[0][0], start[0][1]))
-			cat_env_var(var, str, start, end);
-		(*start)++;
-	}
-	*end = '\"';
-}
-
-char	*cat_intrd_str(t_var *var, char *start, char *end)
+char	*cat_interpreted_str(t_data *data, char *start, char *end)
 {
 	char	*ptr;
 	char	*str;
 
-	str = ft_calloc(var->len + 1, sizeof (char));
+	str = ft_calloc(data->len + 1, sizeof(char));
 	if (!str)
 		return (NULL);
 	while (start < end)
 	{
-		if (*start == '\'' && identify_single_quotes(&start, &ptr))
-			cat_single_qoutes(str, &start, ptr, var->len);
-		else if (*start == '\"' && identify_double_quotes(&start, &ptr))
-			cat_double_qoutes(var, str, &start, ptr);
-		else if (possible_var(var, start[0], start[1]))
-			cat_env_var(var, str, &start, end);
+		if ((*start == '\'' || *start == '\"') && identify_quotes(&start, &ptr))
+			cat_quoted_string(data, str, &start, ptr, *start);
+		else if (possible_var(data, start[0], start[1]))
+			cat_env_var(data, str, &start, end);
 		else if (!lone_dollar_sign(start, end))
-			cat_char_to_str(str, *start, var->len);
+			cat_char_to_str(str, *start, data->len);
 		start++;
 	}
 	return (str);
