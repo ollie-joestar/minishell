a3094c6c231c3a3a2c942396627d0330fbc0f6cb
Ollie Joestar <bigzzzid@gmail.com>
2024-11-12 16:34:13 +0100

    reroute bruh

--- src/reroute.c
+++ src/reroute.c
@@ -6,26 +6,26 @@
 /*   By: oohnivch <@student.42vienna.com>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/11 11:41:48 by oohnivch          #+#    #+#             */
-/*   Updated: 2024/11/12 16:06:49 by oohnivch         ###   ########.fr       */
+/*   Updated: 2024/11/12 16:19:52 by oohnivch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
 // Redirection types:
-//	 - input file & output file
-//	 - input file & output pipe
-//	 - input pipe & output file
-//	 - input pipe & output pipe
-//	 - input file
-//	 - output file
-//	 - input pipe
-//	 - output pipe
-// Input redirection:
+//	 - in file & out file
+//	 - in file & out pipe
+//	 - in pipe & out file
+//	 - in pipe & out pipe
+//	 - in file
+//	 - out file
+//	 - in pipe
+//	 - out pipe
+// in redirection:
 //	 - read from file
 //	 - read from pipe
 //	 - read from STDIN_FILENO
-// Output redirection:
+// out redirection:
 //   - write to file
 //   - write to pipe
 //   - write to STDOUT_FILENO
@@ -34,11 +34,11 @@ static void	rerouteinfile(t_data *data)
 {
 	int	fd;
 
-	fd = open(data->exec->input->file, O_RDONLY);
+	fd = open(data->exec->in->file, O_RDONLY);
 	if (fd == -1)
 	{
 		ft_putstr_fd("minishell: ", STDERR_FILENO);
-		ft_putstr_fd(data->exec->input->file, STDERR_FILENO);
+		ft_putstr_fd(data->exec->in->file, STDERR_FILENO);
 		ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
 		return ;
 	}
@@ -50,14 +50,14 @@ static void	rerouteoutfile(t_data *data)
 {
 	int fd;
 
-	if (data->exec->output->type == REPLACE)
-		fd = open(data->exec->output->file, O_CREAT | O_TRUNC | O_RDWR, 0664);
+	if (data->exec->out->type == REPLACE)
+		fd = open(data->exec->out->file, O_CREAT | O_TRUNC | O_RDWR, 0664);
 	else
-		fd = open(data->exec->output->file, O_CREAT | O_APPEND | O_RDWR, 0664);
+		fd = open(data->exec->out->file, O_CREAT | O_APPEND | O_RDWR, 0664);
 	if (fd == -1)
 	{
 		ft_putstr_fd("minishell: ", STDERR_FILENO);
-		ft_putstr_fd(data->exec->output->file, STDERR_FILENO);
+		ft_putstr_fd(data->exec->out->file, STDERR_FILENO);
 		ft_putstr_fd(": No such file or directory\n", STDERR_FILENO);
 		return ;
 	}
@@ -81,12 +81,12 @@ static void	rerouteoutpipe(t_data *data)
 
 void	reroute(t_data *data)
 {
-	if (data->exec->input)
+	if (data->exec->in)
 		rerouteinfile(data);
-	if (data->exec->output)
+	if (data->exec->out)
 		rerouteoutfile(data);
-	if (!data->exec->input && data->exec->prev)
+	if (!data->exec->in && data->exec->prev)
 		rerouteinpipe(data);
-	if (!data->exec->output && data->exec->next)
+	if (!data->exec->out && data->exec->next)
 		rerouteoutpipe(data);
 }
