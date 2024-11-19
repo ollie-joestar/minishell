a647a9dd218ab038d891ab1cf04c497e165e4595
HanzoK <hanju.soonsik.kim@gmail.com>
2024-11-19 01:08:22 +0100

    Refactored signal.c

--- src/signal.c
+++ src/signal.c
@@ -1,37 +1,35 @@
 
 #include "minishell.h"
 
-sig_atomic_t	g_signal = 0;
+sig_atomic_t g_signal = 0;
 
-void	handle_sigint(int sig)
+void handle_sigint(int sig)
 {
-	g_signal = sig;
-	ioctl(STDIN_FILENO, TIOCSTI, "\n");
-	rl_on_new_line();
-	rl_replace_line("", 0);
+    g_signal = sig;
+    ioctl(STDIN_FILENO, TIOCSTI, "\n");
+    rl_on_new_line();
+    rl_replace_line("", 0);
+    rl_redisplay();
 }
 
-void	save_sigint(int signal)
+void save_sigint(int signal)
 {
-	g_signal = signal;
+    g_signal = signal;
 }
 
-void	sigint_handler_non_interactive_mode(t_data *data)
+void setup_signal_handler(t_data *data, void (*handler)(int))
 {
-	data->sa.sa_handler = save_sigint;
-	sigaction(SIGINT, &data->sa, NULL);
+    data->sa.sa_handler = handler;
+    data->sa.sa_flags = SA_RESTART;
+    sigemptyset(&data->sa.sa_mask);
+    sigaction(SIGINT, &data->sa, NULL);
 }
 
-void	sigint_handler_interactive_mode(t_data *data)
+void setup_signal_handlers(t_data *data, int interactive)
 {
-	data->sa.sa_handler = handle_sigint;
-	sigaction(SIGINT, &data->sa, NULL);
-}
-
-void	setup_signal_handlers(t_data *data)
-{
-	signal(SIGQUIT, SIG_IGN);
-	data->sa.sa_flags = SA_RESTART;
-	sigemptyset(&data->sa.sa_mask);
-	sigint_handler_interactive_mode(data);
+    signal(SIGQUIT, SIG_IGN);
+    if (interactive)
+        setup_signal_handler(data, handle_sigint);
+    else
+        setup_signal_handler(data, save_sigint);
 }
