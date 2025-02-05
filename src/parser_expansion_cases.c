fc9a8cbeb640622902ca4dd9ff785ea3fbaf4fdd
HanzoK <hanju.soonsik.kim@gmail.com>
2025-02-05 14:20:45 +0100

    Making expander more readable (i hope this works and doesn't break everything:))

--- src/parser_expansion_cases.c
+++ src/parser_expansion_cases.c
@@ -29,35 +29,7 @@ int	handle_dollar_exp(t_data *data, t_expander *expander)
 	return (0);
 }
 
-/*int	handle_variable_exp(t_data *data, char *word, t_expander *expander)*/
-/*{*/
-/*	char	var_name[256];*/
-/*	char	*var_value;*/
-/*	size_t	len;*/
-/*	size_t	var_len;*/
-/**/
-/*	var_len = 0;*/
-/*	while (word[expander->index_word] != '\0'*/
-/*		&& (ft_isalnum(word[expander->index_word])*/
-/*			|| word[expander->index_word] == '_'))*/
-/*	{*/
-/*		if (var_len < sizeof(var_name) - 1)*/
-/*			var_name[var_len++] = word[expander->index_word];*/
-/*		(expander->index_word)++;*/
-/*	}*/
-/*	var_name[var_len] = '\0';*/
-/*	var_value = get_env_value(data, var_name);*/
-/*	if (!var_value)*/
-/*		var_value = "";*/
-/*	len = ft_strlen(var_value);*/
-/*	if (resize_result(expander, expander->index_res + len) == -1)*/
-/*		return (-1);*/
-/*	ft_memcpy(&(expander->result[expander->index_res]), var_value, len);*/
-/*	expander->index_res += len;*/
-/*	return (0);*/
-/*}*/
-
-int	handle_variable_exp(t_data *data, char *word, t_expander *expander)
+/*int	handle_variable_exp(t_data *data, char *word, t_expander *expander)
 {
 	char	*var_name;
 	char	*var_value;
@@ -91,13 +63,42 @@ int	handle_variable_exp(t_data *data, char *word, t_expander *expander)
 	var_value = get_env_value(data, var_name);
 	if (!var_value)
 		var_value = "";
-		/*return (ft_free(&var_name), -1);*/
+		//return (ft_free(&var_name), -1);
 	len = ft_strlen(var_value);
 	if (resize_result(expander, expander->index_res + len) == -1)
 	{
 		ft_free(&var_name);
 		return (-1);
 	}
+	ft_memcpy(&(expander->result[expander->index_res]), var_value, len);
+	expander->index_res += len;
+	ft_free(&var_name);
+	return (0);
+}*/
+
+int	handle_variable_exp(t_data *data, char *word, t_expander *expander)
+{
+	char	*var_name;
+	char	*var_value;
+	size_t	name_length;
+	size_t	start;
+	size_t	len;
+
+	start = expander->index_word;
+	while (word[expander->index_word] != '\0'
+		&& (ft_isalnum(word[expander->index_word]) || word[expander->index_word] == '_'))
+		expander->index_word++;
+	name_length = expander->index_word - start;
+	var_name = ft_calloc(name_length + 1, sizeof(char));
+	if (!var_name)
+		return (-1);
+	ft_strlcpy(var_name, word + start, name_length + 1);
+	var_value = get_env_value(data, var_name);
+	if (!var_value)
+		var_value = "";
+	len = ft_strlen(var_value);
+	if (resize_result(expander, expander->index_res + len) == -1)
+		return (ft_free(&var_name), -1);
 	ft_memcpy(&(expander->result[expander->index_res]), var_value, len);
 	expander->index_res += len;
 	ft_free(&var_name);
